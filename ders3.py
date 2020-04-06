c2a2b1bba431109bc5dd59b683beea77b24b193e
Çalgan Aygün <calganaygun@hacettepe.edu.tr>
2020-04-06 18:45:06 +0300

    3. Ders'e ait kodlar eklendi.
    3. Ders'te yazılmış kodlar eklendi.

---
+++ ders3.py
@@ -0,0 +1,35 @@
+"""
+def cikar(a,b):
+    print(a-b)
+cikar(5,8)
+"""
+"""
+def ekranayazdir(a = "Merhaba"):
+    print(a)
+"""
+"""
+def usal(taban,us):
+    global cevap
+    cevap = taban**us
+    # print(cevap)
+    return cevap
+    cevap = 30
+
+print(usal(5,2)+5)
+# print(cevap)
+"""
+"""
+def faktoriyel(x):
+    if x == 1 or x == 0:
+        return 1
+    else:
+        return x*faktoriyel(x-1)
+
+print(faktoriyel(0))
+"""
+
+
+
+
+
+
